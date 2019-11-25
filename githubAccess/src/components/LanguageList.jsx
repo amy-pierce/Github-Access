9741df4b97912b7c655151536bfcb349e0154191
Amy Pierce <34216512+amy-pierce@users.noreply.github.com>
2019-11-25 16:29:03 +0000

    Add files via upload

--- githubAccess/src/components/LanguageList.jsx
+++ githubAccess/src/components/LanguageList.jsx
@@ -1,35 +1,37 @@
 import React from 'react';
 import BarChart from './BarChart';
 let x = ''
-let langs=[]
+let langs = []
 const LanguageList = (props) => {
-  if (props.langslist) {
-    {
-      var totalcount =  Object.entries(props.langslist).map(([key,eachitem]) =>
-        (eachitem.count)).reduce((pv, cv) => pv+cv, 0 ) }
-      return (
-        <div>
-        <div>
-          {
-            Object.entries(props.langslist).map(([key,eachitem]) =>
-              <div><div key={key}>
-                {eachitem.lang} - {Math.round(100*eachitem.count / totalcount)}%
+    if (props.langslist) {
+        {
+            var totalcount = Object.entries(props.langslist).map(([key, eachitem]) =>
+                (eachitem.count)).reduce((pv, cv) => pv + cv, 0)
+        }
+        return (
+            <div>
+                <div>
+                    {
+                        Object.entries(props.langslist).map(([key, eachitem]) =>
+                            <div>
+                                <div key={key}>
+                                    {eachitem.lang} - {Math.round(100 * eachitem.count / totalcount)}%
               </div>
-                    <div>{console.log(x = x + eachitem.count)}</div></div>)}
-        </div>
-              {console.log(x)}
-              <div className='App-header'>
-                  <h2>d3ia dashboard</h2>
-              </div>
-              <div>
-                  <BarChart data={x.split('')} size={[500, 500]} />
-                  {x = ''}
-                 
-              </div>
-</div>      
-            )
+                                <div>{console.log(x = x + eachitem.count + ',')}</div></div>)}
+                </div>
+                {console.log(x)}
+                <div className='App-header'>
+                    <h2>Languages</h2>
+                </div>
+                <div>
+                    <BarChart data={x.split(',')} size={[500, 500]} />
+                    {x = ''}
+
+                </div>
+            </div>
+        )
     } else { return null; }
-    
-  };
+
+};
 
 export default LanguageList;
