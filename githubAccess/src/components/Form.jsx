a178c3c5e26f9f016b68962347a5cd2b544aa896
Amy Pierce <34216512+amy-pierce@users.noreply.github.com>
2019-11-27 15:23:49 +0000

    adding search bar

--- githubAccess/src/components/Form.jsx
+++ githubAccess/src/components/Form.jsx
@@ -1,24 +1,34 @@
 import React from 'react';
+import { Row} from 'react-bootstrap';
+
+
+
 const Form = (props) => {
-    return (
-        <form onSubmit={(event) => props.handleUserFormSubmit(event)}>
-            <label>
-                <p>Search:</p>
-                <input name="username"
-                    type="text"
-                    placeholder="GitHub username"
-                    required
-                    value={props.formData.username}
-                    onChange={props.handleFormChange}
-                />
-            </label>
-            <div>
-                <input
-                    type="submit"
-                    value="Submit"
-                />
-            </div>
-        </form>
-    )
-};
+  return (
+    <Row className="text-right">
+    <form onSubmit={(event) => props.handleUserFormSubmit(event)}> 
+        <label>
+          <input
+            className="form-control"
+            name="username"
+            type="text"
+            placeholder="Github Username"
+            required
+            value={props.formData.username}
+            onChange={props.handleFormChange}
+          />
+        </label>
+        <span>   </span>
+          <input
+            type="submit"
+            value="Submit"
+                  className="btn btn-light"
+                  style={{ backgroundColor: "#D0F0C0" }}
+                  
+          />
+
+    </form>
+    </Row>
+  )};
+
 export default Form;
