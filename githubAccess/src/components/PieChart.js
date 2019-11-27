4621c5fa73ad4c02678d6f953528628f482486ca
Amy Pierce <34216512+amy-pierce@users.noreply.github.com>
2019-11-27 15:24:14 +0000

    add pie chart

---
+++ githubAccess/src/components/PieChart.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Plot from 'react-plotly.js';
+import './app.css'
+
+const PieChart = (props) => {
+
+    return (
+
+        <div class="pie"><Plot data={[
+            {
+                values: props.repoSize,
+                labels: props.repoNames,
+                type: 'pie'
+            }
+        ]}
+            layout={{ width: 700, height: 400 }}
+        />
+        </div>
+    );
+          
+}
+
+
+export default PieChart;
